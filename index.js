994ac685e5236f5e742cc1ea2f1978204923b531
Gabriel <gabrigodoy10@gmail.com>
2023-02-14 13:43:11 -0300

    Pronto

---
+++ index.js
@@ -0,0 +1,15 @@
+let numero1 = prompt("Insira um número inteiro positivo");
+let numero2 = prompt("Insira outro número inteiro positivo");
+
+if (numero1 && numero2 < 0) {
+  alert("Números não positivos, insira novamente");
+  window.location.reload();
+}
+
+let numeros = [];
+
+numeros.push(numero1, numero2);
+
+let divisao = numeros[0] / numeros[1]; //só pro vetor não ficar inútil
+
+alert("Divisão dos dois números: " + divisao);
